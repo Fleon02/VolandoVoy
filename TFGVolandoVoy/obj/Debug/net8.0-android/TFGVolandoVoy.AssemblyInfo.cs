b3fc2e03a837edf089b54793b32579fef40dcd0c
DavidCorredorMiguel <davidcorredormiguel25599@gmail.com>
2024-03-18 16:27:08 +0100

    documentos

--- TFGVolandoVoy/obj/Debug/net8.0-android/TFGVolandoVoy.AssemblyInfo.cs
+++ TFGVolandoVoy/obj/Debug/net8.0-android/TFGVolandoVoy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e02bdd813eea6c2385ea0988dcc153ac10a495f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2c3e56166744027e38f0f90911f8d6fd5dafbfc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
