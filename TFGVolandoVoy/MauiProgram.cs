8e7634826467f54be9bea807e8cfecb3f3c6a05f
Fleon02 <132019583+Fleon02@users.noreply.github.com>
2024-03-22 19:26:28 +0100

    Prueba Supabase

--- TFGVolandoVoy/MauiProgram.cs
+++ TFGVolandoVoy/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Supabase;
 
 namespace TFGVolandoVoy
 {
@@ -15,8 +17,22 @@ namespace TFGVolandoVoy
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Configurar Supabase
+            // Configurar Supabase
+            var url = "https://clfynwobrskueprtvnmg.supabase.co";
+            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsZnlud29icnNrdWVwcnR2bm1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExMjI4OTYsImV4cCI6MjAyNjY5ODg5Nn0.yil-jPNh7m6uk9veYRnnAB2Cjt51lTyCbu18oiluk98";
+
+            var options = new SupabaseOptions
+            {
+                AutoRefreshToken = true,
+                AutoConnectRealtime = true,
+                //SessionHandler = new SupabaseSessionHandler()
+            };
+            var supabase = new Supabase.Client(url, key, options);
+            builder.Services.AddSingleton(supabase);
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ TFGVolandoVoy/paco.cs
@@ -0,0 +1,16 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+using Supabase;
+
+// Dado el siguiente modelo (Paco.cs)
+[Table("paco")]
+public class Paco : BaseModel
+{
+    [PrimaryKey("id_paco")]
+    public int IdPaco { get; set; }
+
+    [Column("nasio_en")]
+    public string? NasioEn { get; set; }
+
+    // Otras propiedades si es necesario
+}
