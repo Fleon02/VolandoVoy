be6c0fde6787dea64f1e809fb3018d718985a5ef
DavidCorredorMiguel <davidcorredormiguel25599@gmail.com>
2024-03-23 11:56:42 +0100

    c

--- TFGVolandoVoy/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TFGVolandoVoy.AssemblyInfo.cs
+++ TFGVolandoVoy/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TFGVolandoVoy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d89dc2c75e2722b77e82d05d3a1b60918f95cd6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e9e4e5d8ef131c537f6cd9b830118a8ce5fd34b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
