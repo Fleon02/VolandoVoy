34c3613b8d89f39b6b3c413300da750eb60ece5d
Fleon02 <132019583+Fleon02@users.noreply.github.com>
2024-03-27 00:26:51 +0100

    Navgeacion Hecha y Limpieza de proyecto

---
+++ TFGVolandoVoy/Vista/InicioSesion.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using TFGVolandoVoy.Modelo;
+
+namespace TFGVolandoVoy
+{
+    public partial class InicioSesion : ContentPage
+    {
+        private readonly Supabase.Client _supabaseClient;
+
+        // Constructor con parámetro
+        public InicioSesion(Supabase.Client supabaseClient)
+        {
+            _supabaseClient = supabaseClient;
+            InitializeComponent();
+        }
+
+        // Constructor sin parámetros
+        public InicioSesion() : this(new Supabase.Client(ConexionSupabase.SUPABASE_URL, ConexionSupabase.SUPABASE_KEY))
+        {
+        }
+
+        // Resto del código de la clase
+        private int count = 0;
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            // Define el correo electrónico y la contraseña para el registro
+            string email = "zxxwaspxxz@gmail.com";
+            string password = "contraseña123";
+
+            try
+            {
+                // Registra al usuario utilizando el correo electrónico y la contraseña predefinidos
+                //var response = await _supabaseClient.Auth.SignUp(email, password);
+
+                await Shell.Current.GoToAsync("//ProvinviaVnt");
+            }
+            catch (Exception ex)
+            {
+                // Si ocurre alguna excepción durante el registro, muestra un mensaje de error
+                await DisplayAlert("Error", $"Error al registrar al usuario: {ex.Message}", "Aceptar");
+            }
+        }
+
+    }
+}
