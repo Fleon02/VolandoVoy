c0f2762cb734e7d03beac355f4f8ee907e0f0ff1
DavidCorredorMiguel <davidcorredormiguel25599@gmail.com>
2024-05-11 11:38:55 +0200

    c

--- TFGVolandoVoy/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TFGVolandoVoy.AssemblyInfo.cs
+++ TFGVolandoVoy/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TFGVolandoVoy.AssemblyInfo.cs
@@ -18,11 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+eb8e8dfded8736fbd708a756519f779669931ecd")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4b1759cd7f2a2631912865ff2a8d50095b521132")]
->>>>>>> main
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b0a932faac5cb055e9e788be0c164d55ecdab131")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
