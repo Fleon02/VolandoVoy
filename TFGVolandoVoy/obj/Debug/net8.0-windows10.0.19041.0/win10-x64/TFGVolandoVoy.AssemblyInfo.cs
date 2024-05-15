0a0abd39d7f94a4e51458c50a50307c1b7d8340c
Fleon02 <132019583+Fleon02@users.noreply.github.com>
2024-05-15 16:53:16 +0200

    METODO DE CAMBIAR CONTRASEÑA

--- TFGVolandoVoy/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TFGVolandoVoy.AssemblyInfo.cs
+++ TFGVolandoVoy/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TFGVolandoVoy.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f9ff9958e38661ab667b3798f6f751fecf58480d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0ad9c3a6cec715944d5b5564286f31f66fd468e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGVolandoVoy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
