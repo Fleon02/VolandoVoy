b344d7e31b1f5f134a521fbf599c0b0233dab261
zzzwaspzzz <98188723+zzzwaspzzz@users.noreply.github.com>
2024-05-18 21:46:00 +0200

    .

--- TFGVolandoVoy/App.xaml.cs
+++ TFGVolandoVoy/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace TFGVolandoVoy
+﻿using TFGVolandoVoy.Converters;
+
+namespace TFGVolandoVoy
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            Resources.Add("StringNullOrEmptyToBoolConverter", new StringNullOrEmptyToBoolConverter());
 
             MainPage = new AppShell();
         }
